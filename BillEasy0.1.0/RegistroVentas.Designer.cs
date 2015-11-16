3896e4f7cfbb8e6895763f1c4db76d650e1ec6b9
Melvin0496 <melvingabriel65@gmail.com>
2015-11-16 18:18:52 -0400

    Agregando interfaz de ventas

---
+++ BillEasy0.1.0/RegistroVentas.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace BillEasy0._1._0
+{
+    partial class RegistroVentas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RegistroVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "RegistroVentas";
+            this.Text = "Registro de ventas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
